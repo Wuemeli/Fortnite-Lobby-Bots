4f1f407a39029814017df4be357ff9090cc6174b
Wuemeli <54443643+Wuemeli@users.noreply.github.com>
2023-01-08 11:42:35 +0100

    Update config.json

--- discordbot.js
+++ discordbot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
+const config = require("./config.json");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -39,4 +40,4 @@ async function checkUserStatus(user) {
   return data.online ? 'Online' : 'Offline';
 }
 
-client.login('your-discord-bot-token-goes-here');
+client.login('config.discordtoken');
