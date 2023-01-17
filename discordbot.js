b5a873dd2d54d76664f21efd2b95fcd4efef2de5
Wuemeli <wuemeli@gmail.com>
2023-01-17 09:36:15 +0000

    Update

--- discordbot.js
+++ discordbot.js
@@ -9,27 +9,39 @@ const client = new Client({
 //Ready
 
 client.on("ready", () => {
-  client.user.setActivity("type {help");
+  client.user.setActivity("Type {help");
   console.log("I am ready!");
 });
 
 //Command Handling
 
 client.on("messageCreate", (message) => {
+//Help
   if (message.content.startsWith("{help")) {
-    message.channel.send("commands:\n{featured\n{daily\n{specialFeatured");
+    message.channel.send("commands:\n{featured\n{daily\n{specialFeatured\n{status\n{list");
+//Item Shop
   } if (message.content.startsWith("{daily")) {
     fortniteStuff("daily", message);
   } if (message.content.startsWith("{featured")) {
     fortniteStuff("featured", message);
   } if (message.content.startsWith("{specialFeatured")) {
     fortniteStuff("specialFeatured", message);
+//Fortnite Bot Status and List
+//Status
+  } if (message.content.startsWith("{status")) {
+    message.channel.send("All Fortnite Bots Are Online.");
+//List
+  } if (message.content.startsWith("{list")) {
+    message.channel.send("Fortnite Bots:\nFNKarmaBot1\nFNKarmaBot2\nFNKarmaBot3\nFNKarmaBot4\nFNKarmaBot5\nFNKarmaBot6\nFNKarmaBot7\nFNKarmaBot8\nFNKarmaBot9\nFNKarmaBot10");
+//Invalid Command
+  } if (message.content.startsWith("{")) {
+    message.channel.send("Invalid Command");
   }
 });
 
 //Login
 
-client.login("config.dcbottoken");
+client.login(config.dcbottoken);
 
 //API
 
@@ -58,8 +70,7 @@ const parseFortniteShop = (object, message) => {
     let items = {};
     const entries = object["entries"];
     for (let key in entries) {
-        for (let i in entries[key]["items"]) {
-            // singular items: 
+        for (let i in entries[key]["items"]) { 
             if (entries[key]["bundle"] == null) {
                 let section = entries[key]["section"]["name"];
                 if (items[section] === undefined) {
